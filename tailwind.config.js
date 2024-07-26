11f43c940e060b0c92eb38e735a80cb76b0cf264
marcosgabriel <marcosgabriel.mg318@gmail.com>
2024-07-26 01:33:15 -0300

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dmsans': ['DM Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
