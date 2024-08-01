213c55ada6d101613d48f76a366805bf51594b37
Gabriel <marcosgabriel.mg318@gmail.com>
2024-08-01 20:17:12 -0300

    add Estrutura da landing page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const menuIcon = document.querySelector(".menu-icon");
-const menu = document.querySelector(".menu")
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
 
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.remove("bg-transparent");
+    header.classList.add("bg-black");
+  } else {
+    header.classList.remove("bg-black");
+    header.classList.add("bg-transparent");
+  }
+});
 
-menuIcon.addEventListener('click', ()=>{
-    menu.classList.toggle('-top-80')
-})
+menuIcon.addEventListener("click", () => {
+  if (
+    menu.classList.contains("-top-[260px]") &&
+    menu.classList.contains("-z-10")
+  ) {
+    menu.classList.remove("-top-[260px]");
+    menu.classList.add("top-14");
+    menu.classList.toggle("-z-10");
+  } else {
+    menu.classList.remove("top-14");
+    menu.classList.add("-top-[260px]");
+    menu.classList.toggle("-z-10");
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*{html,js}"],
   theme: {
     extend: {
+      colors: {
+        "initial-font": "#D2AC68",
+        "middle-font": "#EEE0B2",
+        "final-font": "#B6781E",
+        "btn-hero": "#D1B96E",
+      },
+      backgroundImage: {
+        "logo-header": "url('/src/images/logo_dourada.png')",
+        "bg-hero": "url('/src/images/background-hero.jpg')",
+      },
       fontFamily: {
-        'dmsans': ['DM Sans', 'sans-serif'],
-      }
+        dmsans: ["DM Sans", "sans-serif"],
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [addDynamicIconSelectors()],
+};
